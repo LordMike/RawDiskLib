a94554a8ade067863ae32d83f500459003e28398
Michael Bisbjerg <michael@mbwarez.dk>
2016-11-08 01:32:21 +0100

    Minifiy dependency list

---
+++ RawDiskLib/Helpers/Debug.cs
@@ -0,0 +1,19 @@
+﻿#if NETCORE
+// ReSharper disable once CheckNamespace
+namespace System.Diagnostics
+{
+    public static class Debug
+    {
+        public static void WriteLine(string message)
+        {
+            // Do nothing
+        }
+
+        public static void Assert(bool condition)
+        {
+            if (!condition)
+                throw new Exception("Assertion failure");
+        }
+    }
+}
+#endif
